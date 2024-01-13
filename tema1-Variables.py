f65666795fb746018b1d9f5bb9048ccd87429419
IDGS-803-21000029 <21000029@alumnos.utleon.edu.mx>
2024-01-12 18:49:39 -0600

    JIGC 12.01.2024 Tuplas

---
+++ tema1-Variables.py
@@ -0,0 +1,26 @@
+
+#Nombre de Variables
+
+'''
+comentarios
+multi
+linea
+'''
+
+numero1 = "23"
+numero2="10"
+nombre="Domingo"
+correo="domigo@gmail.com"
+
+print(numero1 + numero2)
+
+#Conversion forzosa entre tipos de datos
+print("La suma es: ")
+print(int(numero1) + int(numero2))
+
+sueldo = "938.284"
+valorDecimal = float(sueldo)
+print(sueldo)
+
+edad = 283
+print(len(str(edad)))
